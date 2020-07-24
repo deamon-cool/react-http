c600264cadba94473e438d7689ba21d0dada6299
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-24 21:04:17 +0200

    made post selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import axios from 'axios';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     };
 
     componentDidMount() {
@@ -27,13 +28,18 @@ class Blog extends Component {
             });
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+
     render() {
         const posts = this.state.posts.map(post => {
             return <Post
                 key={post.id}
                 title={post.title}
                 author={post.author}
-            />;
+                clicked={() => this.postSelectedHandler(post.id)} />;
         });
 
         return (
@@ -42,7 +48,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />

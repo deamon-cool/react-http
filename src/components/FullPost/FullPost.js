cc69a8124cda22e80f6a10a69732fcea6235733f
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-27 21:14:21 +0200

    added fetching clicked post

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    };
+
+    componentDidUpdate() {
+        if (this.props.id) {
+            axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(res => {
+                this.setState({loadedPost: res.data});
+            });
+        }
+
+    }
+
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
         if (this.props.id) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>

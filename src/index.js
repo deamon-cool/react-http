a396aa35e8379538812c2b9fcb0fb0e622808963
daemon-cool <damian.damian.tofik@gmail.com>
2020-07-28 17:18:16 +0200

    added global error handlers for all request from client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(request => {
+    console.log(request);
+
+    return request;
+}, err => {
+    console.log(err);
+
+    return Promise.reject(err);
+});
+
+axios.interceptors.response.use(response => {
+    console.log(response);
+
+    return response;
+}, err => {
+    console.log(err);
+
+    return Promise.reject(err);
+});;
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

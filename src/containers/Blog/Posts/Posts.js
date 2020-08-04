a085c97c98ac3dab41dfae81c95e88a029f69527
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-04 09:32:15 +0200

    imported

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import axios from 'axios';
 
+import Post from '../../../components/Post/Post';
+
 class Posts extends React.Component {
     state = {
         posts: [],

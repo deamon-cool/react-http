f067889d46e048235f1e2009a127bbc2a5ac83fe
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-04 10:33:20 +0200

    Changed links <a/> for <Link/> component from router

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -12,12 +13,17 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href='/'>Home</a></li>
-                            <li><a href='/new-post'>New Post</a></li>
+                            <li><Link to='/'>Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Route path='/' exact render={() => <h1>Home</h1>} />
+                <Route path='/' exact component={Posts} />
+                <Route path='/new-post' component={NewPost} />
             </div>
         );
     }

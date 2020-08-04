6f769e5886c49c91f5dafd3fd31e3d6beab4d3d7
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-04 09:23:54 +0200

    created ne container Posts

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Posts extends React.Component {
+
+    render() {
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;

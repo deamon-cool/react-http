2754a8ae7ec97697e9a2b2c3bd2bbc68626222a5
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-04 09:48:14 +0200

    added route to home

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -16,7 +17,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path='/' exact render={() => <h1>Home</h1>} />
             </div>
         );
     }

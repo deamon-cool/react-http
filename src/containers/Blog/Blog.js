1d2189152827f51921444ff2fdb9df7e8d69f754
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-04 09:25:24 +0200

    deleted and added to posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -33,18 +33,6 @@ class Blog extends Component {
 
 
     render() {
-        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
-
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
-
         return (
             <div className='Blog'>
                 <header>
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -3,6 +3,18 @@ import React from 'react';
 class Posts extends React.Component {
 
     render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />;
+            });
+        }
+
         return (
             <section className="Posts">
                 {posts}

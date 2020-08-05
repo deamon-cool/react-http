708869735c7edc39744e016b25ea224a79ea084f
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-05 19:09:41 +0200

    changed active name class

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -13,7 +13,14 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink to='/' exact>Home</NavLink></li>
+                            <li><NavLink
+                                to='/'
+                                exact
+                                activeClassName='my-active'
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Home</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',

0523e28eb540d270c00198ff7dbc8ba9ad644f21
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-05 19:26:28 +0200

    added route path with id parameter. Thanks to that in the future will be avaliable load clicked post in FullPost component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,6 +4,8 @@ import { Route, NavLink } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import Post from './FullPost/FullPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -31,6 +33,7 @@ class Blog extends Component {
                 </header>
                 <Route path='/' exact component={Posts} />
                 <Route path='/new-post' component={NewPost} />
+                <Route path='/:id' component={FullPost} />
             </div>
         );
     }

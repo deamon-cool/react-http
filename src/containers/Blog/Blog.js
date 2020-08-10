f30d27c32001de4219c3bf2e6ac771175fa19f62
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-10 17:03:25 +0200

    changed due to used Switch module. In this case Switch allows choosing only first and one pass url, so only one Route is rendering

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -31,8 +31,10 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Route path='/' exact component={Posts} />
-                <Route path='/new-post' component={NewPost} />
-                <Route path='/post/:id' component={FullPost} />
+                <Switch>
+                    <Route path='/new-post' component={NewPost} />
+                    <Route path='/:id' component={FullPost} />
+                </Switch>
             </div>
         );
     }

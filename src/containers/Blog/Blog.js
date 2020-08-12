0208462c8629e342a7d5ff9a59323a09feea22e4
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-12 17:08:54 +0200

    GUARD for authorization: Added authorization state to /new-post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,6 +6,9 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+    state = {
+        auth: false
+    };
 
     render() {
         return (
@@ -30,7 +33,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    <Route path='/new-post' component={NewPost} />
+                    {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null}
                     <Route path='/posts' component={Posts} />
                     <Redirect from='/' to='/posts' />
                 </Switch>

b638da2e43482ecc95960da98ee6b92272fca302
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-12 17:39:39 +0200

    Optimization network: imported async component that allows downloading chunks of code with needed component content

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,11 +3,16 @@ import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/ansyncComponent';
+// import NewPost from './NewPost/NewPost';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     };
 
     render() {
@@ -33,7 +38,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path='/new-post' component={NewPost} /> : null}
+                    {this.state.auth ? <Route path='/new-post' component={AsyncNewPost} /> : null}
                     <Route path='/posts' component={Posts} />
                     <Route render={() => <h1>Not Found</h1>} />
                     {/* <Redirect from='/' to='/posts' /> */}

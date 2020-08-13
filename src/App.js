909e56cacd91c22f64bf312386a427ef0f957c89
daemon-cool <damian.damian.tofik@gmail.com>
2020-08-13 20:57:12 +0200

    added base name for tests. It allow add to the URL path base name part

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename='/base-name'>
         <div className="App">
           <Blog />
         </div>
